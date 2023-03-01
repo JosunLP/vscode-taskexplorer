bb90f2ab6e9189707e1c4dab8156aba9e8186543
Scott Meesseman <spmeesseman@gmail.com>
2023-03-01 01:47:17 -0500

    progress: task monitor react app webview - flyout menu

--- src/webview/app/monitor/cmp/menu.tsx
+++ src/webview/app/monitor/cmp/menu.tsx
@@ -17,15 +17,55 @@ export class AppMenu extends React.Component<ReactProps>
 
     override render()
     {
+        const cls = this.props.menuVisibility ? "show" : "hide";
         return (
-            <div id="te-monitor-id-flyout-menu"
-                 onMouseDown={this.props.handleMouseDown}
-                 className={this.props.menuVisibility ? "show" : "hide"}>
-                <h2><a href="#">Home</a></h2>
-                <h2><a href="#">About</a></h2>
-                <h2><a href="#">Contact</a></h2>
-                <h2><a href="#">Search</a></h2>
+            <div id="te-monitor-flyout-menu-id" onMouseDown={this.props.handleMouseDown} className={cls}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="te-monitor-flyout-menu-title-td">
+                                <span className="te-title far fa-gears" /> &nbsp;
+                                <span className="te-title te-monitor-flyout-menu-title">Task Explorer</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="te-monitor-flyout-menu-section">
+                                Timer Mode
+                            </td>
+                        </tr>
+                        <tr>
+                            <table className="te-monitor-flyout-menu-section-items">
+                                <tbody>
+                                    <tr>
+                                        <td className="te-monitor-flyout-menu-section-item-td">
+                                            <span className="far fa-chevron-right" /> <span className="te-monitor-flyout-menu-section-item">Hide</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="te-monitor-flyout-menu-section-item-td">
+                                            <span className="far fa-chevron-right" /> <span className="te-monitor-flyout-menu-section-item">MM:SS</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="te-monitor-flyout-menu-section-item-td">
+                                            <span className="far fa-chevron-right" /> <span className="te-monitor-flyout-menu-section-item">MM:SS:MS</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
+
+    setTimerMode = () =>
+    {
+
+    };
 }
