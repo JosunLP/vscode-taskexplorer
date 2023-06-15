27db90a7d45ff3323cbc6dbf21acc9ceeb46e2e8
Scott Meesseman <spmeesseman@gmail.com>
2023-06-15 11:57:22 -0400

    project: cleanup

--- webpack.plugin.js
+++ webpack.plugin.js
@@ -214,12 +214,12 @@ const wpPlugin =
 	{
 		/** @type {CopyPlugin | undefined} */
 		let plugin;
+		const /** @type {CopyPlugin.Pattern[]} */patterns = [],
+			  psx__dirname = __dirname.replace(/\\/g, "/"),
+			  psxBasePath = env.basePath.replace(/\\/g, "/"),
+			  psxBaseCtxPath = path.posix.join(psxBasePath, "res");
 		if (env.build === "webview")
 		{
-			const /** @type {CopyPlugin.Pattern[]} */patterns = [],
-				  psx__dirname = __dirname.replace(/\\/g, "/"),
-				  psxBasePath = env.basePath.replace(/\\/g, "/");
-
 			apps.filter(app => fs.existsSync(path.join(env.basePath, app, "res"))).forEach(
 				app => patterns.push(
 				{
@@ -228,19 +228,42 @@ const wpPlugin =
 					context: path.posix.join(psxBasePath, app, "res")
 				})
 			);
-
 			if (fs.existsSync(path.join(env.basePath, "res")))
 			{
 				patterns.push({
 					from: path.posix.join(psxBasePath, "res", "*.*"),
 					to: path.posix.join(psx__dirname, "res", "webview"),
-					context: path.posix.join(psxBasePath, "res")
+					context: psxBaseCtxPath
 				});
 			}
-
-			if (patterns.length > 0) {
-				plugin = new CopyPlugin({ patterns: patterns });
-			}
+		}
+		else if (wpConfig.mode === "production")
+		{
+			const psx__dirname_info = path.posix.normalize(path.posix.resolve(psx__dirname, "..", "vscode-taskexplorer-info"));
+			patterns.push(
+			{
+				from: path.posix.join(psxBasePath, "res", "img", "*.*"),
+				to: path.posix.join(psx__dirname_info, "res", "img"),
+				context: psxBaseCtxPath
+			},
+			{
+				from: path.posix.join(psxBasePath, "res", "readme", "*.*"),
+				to: path.posix.join(psx__dirname_info, "res", "readme"),
+				context: psxBaseCtxPath
+			},
+			{
+				from: path.posix.join(psxBasePath, "*.md"),
+				to: path.posix.join(psx__dirname_info),
+				context: psxBaseCtxPath
+			},
+			{
+				from: path.posix.join(psxBasePath, "LICENSE*"),
+				to: path.posix.join(psx__dirname_info),
+				context: psxBaseCtxPath
+			});
+		}
+		if (patterns.length > 0) {
+			plugin = new CopyPlugin({ patterns: patterns });
 		}
 		if (!plugin) {
 			plugin = /** @type {CopyPlugin} */(/** @type {unknown} */(undefined));
