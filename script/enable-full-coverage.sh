eb16f4855ef0aa91cc49896540937f2074efc971
Scott Meesseman <spmeesseman@gmail.com>
2023-07-02 12:51:00 -0400

    build: progress wp config breakdown

--- script/enable-full-coverage.sh
+++ script/enable-full-coverage.sh
@@ -8,13 +8,20 @@ if [ ! -z $1 ] ; then
     ENABLE=0
 fi
 
+TABS=$(find . -maxdepth 1 -name "package.json"| xargs egrep ".*\t{2,}")
+
 if [ $ENABLE = 1 ]; then
-  sed -i ':a;N;$!ba;s/"activationEvents": \[[ \r\n\t]*"\*"/"activationEvents": []/g' package.json
-  sed -i ':a;N;$!ba;s/]\n[ ]*],/],/g' package.json
+  if [ -z "$TABS" ]; then
+    sed -i ':a;N;$!ba;s/"activationEvents": \[[ \r\n\t]*"\*"/"activationEvents": []/g' package.json
+    sed -i ':a;N;$!ba;s/]\n[ ]*],/],/g' package.json
+  fi
+  if [ ! -z "$TABS" ]; then
+    sed -i ':a;N;$!ba;s/"activationEvents": \[[ \r\n\t]*"\*"/"activationEvents": []/g' package.json
+    sed -i ':a;N;$!ba;s/]\n[\t]*],/],/g' package.json
+  fi
 fi
 
 if [ $ENABLE = 0 ]; then
-  TABS=$(find . -maxdepth 1 -name "package.json"| xargs egrep ".*\t{2,}")
   if [ -z "$TABS" ]; then
     sed -i 's/"activationEvents": \[]/"activationEvents": [\n        "*"\n    ]/g' package.json
   fi
