6cd7486a2c8d0f47dbdb56641459e868bc77fd38
Scott Meesseman <spmeesseman@gmail.com>
2023-07-15 17:09:06 -0400

    fix: home view displays funky info when lic server error

--- src/webview/view/homeView.ts
+++ src/webview/view/homeView.ts
@@ -32,16 +32,17 @@ export class HomeView extends TeWebviewView<State>
 	{
 		const isLic = this.wrapper.licenseManager.isLicensed,
 			  isTrial = this.wrapper.licenseManager.isTrial,
+			  isError = this.wrapper.licenseManager.isErrorState,
 			  days = this.wrapper.licenseManager.statusDays;
     	html = html.replace("#{taskCounts.length}", this.wrapper.treeManager.tasks.length.toString())
 				   .replace("#{taskCounts.today}", this.wrapper.usage.getTodayCount("").toString())
 				   .replace("#{license.sessionIconTip}", "Session status - Not Verified")
-				   .replace("#{license.statusIconTip}", "License status - " + (isTrial ? "Unlocked (Trial)" : (isLic ? "Unlocked" : "Locked")))
-				   .replace("#{license.sessionIconCls}", "fal fa-" + (isTrial ? "user-slash" : (isLic ? "user-slash" : "user-slash")))
-				   .replace("#{license.statusIconCls}", "fal fa-" + (isTrial ? "unlock te-color-favorite-yellow" : (isLic ? "unlock te-color-ok-green" : "lock te-color-failure-red")))
+				   .replace("#{license.statusIconTip}", "License status - " + (isError || isTrial ? "Unlocked (Trial)" : (isLic ? "Unlocked" : "Locked")))
+				   .replace("#{license.sessionIconCls}", "fal fa-" + (isTrial || isError ? "user-slash" : (isLic ? "user-slash" : "user-slash")))
+				   .replace("#{license.statusIconCls}", "fal fa-" + (isTrial || isError ? "unlock te-color-favorite-yellow" : (isLic ? "unlock te-color-ok-green" : "lock te-color-failure-red")))
 				   .replace("#{license.status}", this.wrapper.licenseManager.statusDescription)
-				   .replace("#{license.statusDays}", isTrial || isLic ? ` (${days})` : "")
-				   .replace("#{license.statusTip}", isTrial ? `${days} days left in trial` : (isLic ? `${days} days left before renewal` : ""));
+				   .replace("#{license.statusDays}", !isError && (isTrial || isLic) ? ` (${days})` : "")
+				   .replace("#{license.statusTip}", !isError ? (isTrial ? `${days} days left in trial` : (isLic ? `${days} days left before renewal` : "")) : "A license server error occurred, full access granted");
 		return html;
 	};
 
