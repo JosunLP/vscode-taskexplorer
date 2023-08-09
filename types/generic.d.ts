3abb585a88af83e0480e30faa67acf97662eebcb
Scott Meesseman <spmeesseman@gmail.com>
2023-08-09 12:10:56 -0400

    project: add some nice helper typing defs

--- types/generic.d.ts
+++ types/generic.d.ts
@@ -1,5 +1,13 @@
 
 /**
+ * Generic Typings
+ */
+
+export type AsArray<T = any> = T extends any[] ? T : [T];
+export type PickByType<T, Value> = { [P in keyof T as T[P] extends Value | undefined ? P : never]: T[P] };
+export type RequireKeys<T extends object, K extends keyof T> = Required<Pick<T, K>> & Omit<T, K>;
+
+/**
  * Base Types
  */
 
